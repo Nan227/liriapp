a9653ca549ee04a6c6e98fa7986dcac8f83673f0
unknown <pp_nancy@hotmail.com>
2019-01-23 23:13:38 -0500

    updated Spotify ID and added liri.js

--- keys.js
+++ keys.js
@@ -1,9 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_3bbbbbdeceaf43bd9eb52be174bd2b66 ,
-  secret: process.env.SPOTIFY_f119fb7824ee465ebc380bee2536d2f4
-
-  // secret: f119fb7824ee465ebc380bee2536d2f4
-  // Client ID 3bbbbbdeceaf43bd9eb52be174bd2b66
+  id: process.env.SPOTIFY_ID ,
+  secret: process.env.SPOTIFY_SECRET
 };
